3e9ef877142017150253a88dbcbc61161a751c83
Shauntel <shauntel.phillips@yahoo.com>
2024-02-03 12:24:20 -0600

    “Update

--- python-api-challenge/api_keys.py
+++ python-api-challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "a60dd4a9a40f177d105920d31cb2170f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "AIzaSyAMYOq-V-YbYt-p0LjVRcPWwJ-9b7lSozY"
