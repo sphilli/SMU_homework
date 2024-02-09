8c62184604bb8115ff4efc0f3c54d11607bcd35c
Shauntel <shauntel.phillips@yahoo.com>
2024-02-08 19:32:15 -0600

    “Update

---
+++ 9-SQL-challenge/EmployeeSQL/Query.sql
@@ -0,0 +1,78 @@
+-- Problem_1 Emp Salaries
+SELECT  emp.emp_no,
+        emp.last_name,
+        emp.first_name,
+        emp.sex,
+        sal.salary
+FROM employees as emp
+    LEFT JOIN salaries as sal
+    ON (emp.emp_no = sal.emp_no)
+ORDER BY emp.emp_no;
+
+-- Problem_2 Emp w/ hire date = 1986
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- Problem_3 Dpt Mgr
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        e.last_name,
+        e.first_name
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN employees AS e
+        ON (dm.emp_no = e.emp_no);
+
+
+--Problem_4 Emp Dpt
+SELECT  e.emp_no,
+        e.last_name,
+        e.first_name,
+        d.dept_name
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+ORDER BY e.emp_no;
+
+-- Problem_5 Emp w/ first ="Hercules"/ last begins with "B"
+SELECT first_name, last_name, birth_date, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- Problem_6 Sales dpt emp
+SELECT  e.emp_no,
+        e.last_name,
+        e.first_name,
+        d.dept_name
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales'
+ORDER BY e.emp_no;
+
+-- Problem_7 Sales/Dev Dpt Emp
+SELECT  e.emp_no,
+        e.last_name,
+        e.first_name,
+        d.dept_name
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development')
+ORDER BY e.emp_no;
+
+-- frequency of employee last 
+SELECT last_name, COUNT(last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC;
